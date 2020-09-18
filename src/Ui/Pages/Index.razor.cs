36f8c62ccc9eeabe24bda13753b140a5aeeb4e04
Benjamin Sampica <benjamin.sampica@gmail.com>
2020-09-17 20:48:06 -0500

    Deleted counter and added base google maps

---
+++ src/Ui/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Ui.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject] private IJSRuntime JsRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JsRuntime.InvokeVoidAsync("initialize", null);
+        }
+    }
+}
---
+++ src/Ui/wwwroot/js/googleMaps.js
@@ -0,0 +1,19 @@
+﻿function initialize() {
+    const options = {
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: { lat: 40.597681, lng: -95.050860 }
+    };
+    let map = new google.maps.Map(document.getElementById
+        ("google-map"), options);
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        let marker = new google.maps.Marker({ position: pos, map: map });
+        map.setCenter(pos);
+    });
+}
