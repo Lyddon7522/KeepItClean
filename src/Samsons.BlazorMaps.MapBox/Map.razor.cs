ccea91e5ebcc15d8c9bf544d1754121b48800a87
Ben Sampica <benjamin.sampica@gmail.com>
2023-02-04 13:51:55 -0600

    Initial commit

---
+++ src/Samsons.BlazorMaps.MapBox/Map.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Samsons.BlazorMaps.MapBox;
+
+public partial class Map
+{
+    [Inject] public required IJSRuntime JSRuntime { get; set; }
+
+    private IJSObjectReference _mapModule = null!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _mapModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Samsons.BlazorMaps.Mapbox/Map.razor.js");
+        }
+
+        await _mapModule.InvokeVoidAsync("createMapBoxMap");
+    }
+}
