ea4e19ca14e9ff909b4cabfb512350a69b61ecf8
mlyddon <mlyddon@agleader.com>
2023-03-01 15:41:19 -0600

    Added async call to geolocation api
    Updates the map location after calling the geolocation api and returning a promise with the coordinates.
    
    Adds a marker to your current location

--- src/Samsons.BlazorMaps.MapBox/Map.razor.js
+++ src/Samsons.BlazorMaps.MapBox/Map.razor.js
@@ -1,11 +1,10 @@
-﻿export function createMapBoxMap(position) {
-    
+export async function createMapBoxMap() {
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
-        center: [position.coords.longitude, position.coords.latitude], // starting position [lng, lat]
-        zoom: 15 // starting zoom
+        center: [-93.6250, 41.5868], // starting position [lng, lat]
+        zoom: 10 // starting zoom
     });
 
     map.addControl(
@@ -17,21 +16,31 @@
             showUserHeading: true
         })
     );
+    
+    const options = {
+        maximumAge: 10000,
+        timeout: 5000,
+    }
+    
+    try {
+        const position = await getPosition(options);
+        map.flyTo({
+            center: [position.coords.longitude, position.coords.latitude],
+            zoom: 12,
+            speed: 0.5,
+            curve: 1,
+        });
+        const marker = new mapboxgl.Marker({
+            draggable: true
+        }).setLngLat([position.coords.longitude, position.coords.latitude])
+            .addTo(map);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
-export function getGeolocation() {
-  
-    function success(position) {
-        createMapBoxMap(position);
-    }
-
-    function error() {
-        alert("Unable to retrieve your location");
-    }
-
-    if (!navigator.geolocation) {
-        alert("Geolocation is not supported by your browser");
-    } else {
-        navigator.geolocation.getCurrentPosition(success, error);
-    }
+export function getPosition(options){
+    return new Promise((resolve, reject) =>
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+    );
 }
