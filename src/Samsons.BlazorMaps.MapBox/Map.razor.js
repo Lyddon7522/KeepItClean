a32172eff2f6b179a254b096d98510d1344285c8
mlyddon <mlyddon@agleader.com>
2023-03-01 13:04:53 -0600

    Added Geolocate Control and Satellite view

--- src/Samsons.BlazorMaps.MapBox/Map.razor.js
+++ src/Samsons.BlazorMaps.MapBox/Map.razor.js
@@ -2,8 +2,16 @@
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
+        style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+        center: [-93.6250, 41.5868], // starting position [lng, lat]
         zoom: 9 // starting zoom
     });
+
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+        })
+    );
 }
