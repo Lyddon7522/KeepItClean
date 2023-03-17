3e4410cbd89d9be75b06bf556488034e5d0beafa
mlyddon <mlyddon@agleader.com>
2023-03-17 15:53:42 -0500

    Added FlyTo to map

--- src/KeepItClean.Client/Pages/Index.razor.cs
+++ src/KeepItClean.Client/Pages/Index.razor.cs
@@ -4,10 +4,20 @@ namespace KeepItClean.Client.Pages;
 
 public partial class Index
 {
+    private static readonly LngLatLike _coordinates = new LngLatLike { Longitude = -93.6250, Latitude = 41.5868 }
+        ; 
     private MapOptions _options = new MapOptions()
     {
-        Center = new LngLatLike { Longitude = -93.6250, Latitude = 41.5868 },
+        Center = _coordinates,
         Zoom = 10,
         Style = "mapbox://styles/mapbox/satellite-streets-v12"
+    };
+
+    private FlyToOptions _flyToOptions = new FlyToOptions()
+    {
+        Center = _coordinates,
+        Zoom = 12,
+        Speed = 1.2,
+        Curve = 1
     };
 }
